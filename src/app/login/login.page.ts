782f6379b35ad5d790c55c76e262e755acb448fa
sukhvir <sukhvir.ameotech@gmail.com>
2022-12-28 15:54:07 +0530

    Refactored design

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { 
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    })
+  }
 
   ngOnInit() {
+  }
+
+  login() {
+    console.log(this.loginForm.value);
   }
 
 }
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  isSubmitted = false;
+
+  constructor(public formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    })
+  }
 
   ngOnInit() {
+  }
+
+  submitForm() {
+    console.log(this.registerForm.value);
+    // this.isSubmitted = true;
+    // if (!this.registerForm.valid) {
+    //   console.log('Please provide all the required values!')
+    //   return false;
+    // } else {
+    //   console.log(this.registerForm.value)
+    // }
+    return;
   }
 
 }
